11ce834609f9296c5a560373bad5fd37d4cdb6ac
Tony <tonybarton23@gmail.com>
2025-05-15 10:00:30 -0400

    Add hydra automation script SFTP connection added to hydra automation script

---
+++ Hydra Automation/main.py
@@ -0,0 +1,15 @@
+import pysftp
+import getpass
+import subprocess
+
+hostname = "columbo"
+username = input("Username: ")
+password = getpass.getpass("Password: ")
+
+try:
+    with pysftp.Connection(hostname, username=username, password=password) as sftp:
+        print("Connection Successful")
+except pysftp.ConnectionException as e:
+    print(f"Connection Error: {e}")
+except IOError as e:
+    print(f"File operation error: {e}")
